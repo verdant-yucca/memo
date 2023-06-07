652297fa2f8b8c1da602dc112377a2e56889d62f
Roman Davydenko <Deadshot323rd@yandex.ru>
2023-06-07 20:18:25 +0800

    feat/memo-1: create project

---
+++ main.js
@@ -0,0 +1,20 @@
+/** Комментарий, который начинается с двух звездочек - JSDoc комментарий, отображается в подсказке Visual Studio */
+
+let board = document.querySelector("#board");
+let score = document.querySelector("#atemptNumOutput");
+let startGameButton = document.querySelector("#startGame");
+
+let gm = new GameManager(board, score);
+gm.startGame();
+
+board.addEventListener("click", function (e) {
+  console.log(e.target)
+    let clickedCard = e.target.connectedCard;
+    if (clickedCard) {
+        gm.selectCard(clickedCard);
+    }
+});
+
+startGameButton.addEventListener("click", function () {
+    gm.startGame();
+});
